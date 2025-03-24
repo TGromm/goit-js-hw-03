c584eac95b37e4c0e352279ddf1c4f1aa8dddd38
Tanya Gromova <peepzebra@gmail.com>
2025-03-24 15:32:15 +0200

    hw-js-3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const filteredNumb = [];
+  for (const number of numbers) {
+    if (number > value) {
+      filteredNumb.push(number);
+    }
+  }
+  return filteredNumb;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
